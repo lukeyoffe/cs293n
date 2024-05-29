e9b21fd67fdd26d2c7cc0d0e615db132451f7861
Ziv Weissman <ziv@ucsb.edu>
2024-05-29 12:00:23 -0700

    training data for 0.05s

--- model/get_tensors.py
+++ model/get_tensors.py
@@ -29,10 +29,10 @@ speed_test_results = []  # List of floats
 for index, row in tqdm(sample.iterrows(), total=len(sample)):
     speed_test_results.append(row['MeanThroughputMbps'])
     packet_capture_data.append(calculate_features(pcap_dir=("../data_aggregation/pcaps_old/" + row['id'] + ".pcap.gz"), 
-                                                  time_delta_s=0.01, 
+                                                  time_delta_s=0.05, 
                                                   start_time_str=row['StartTime'], 
                                                   client_ip=row['ClientIP'], 
-                                                  total_time_s=10))
+                                                  total_time_s=5))
 
 # Convert data to tensors
 data_tensors = []
